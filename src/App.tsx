f543ee72266fb6265b20251307a8d2be8da0d054
AkaisiTakumi <115193708+AkaisiTakumi@users.noreply.github.com>
2025-08-03 17:26:57 +0900

    [feat] 入力した単語の末尾が'ん'のときエラーを表示
    #2
    入力した単語の末尾が'ん'のときエラーを表示
    
    ひとつ前の単語は{lastText}です
    をゲーム中と末尾が'ん'のとき表示する
    
    textboxをゲーム中にのみ表示する
    
    ひとつ前の単語の末尾と入力した単語の先頭が一致しません
    をそのときのみ表示する
    
    入力した単語の末尾が'ん'でした ゲームを終了します
    をそのときのみ表示する
    
    コメント文を追加

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App() {
 	// スタートボタンの状態
 	const [isStart, setIsStart] = useState(false);
 
+	// 入力した単語の末尾が'ん'のとき、true
+	const [nError, setNError] = useState(false);
+
 	// しりとりの直前の単語
 	const [lastText, setLastText] = useState("しりとり");
 
@@ -15,10 +18,16 @@ function App() {
 				isStart={isStart}
 				setIsStart={setIsStart}
 				setLastText={setLastText}
+				setNError={setNError}
 			/>
-			{isStart && (
-				<TextUpdate lastText={lastText} setLastText={setLastText} />
-			)}
+			<TextUpdate
+				isStart={isStart}
+				setIsStart={setIsStart}
+				nError={nError}
+				setNError={setNError}
+				lastText={lastText}
+				setLastText={setLastText}
+			/>
 		</>
 	);
 }
@@ -27,15 +36,18 @@ function App() {
 function StartResetButton({
 	isStart,
 	setIsStart,
+	setNError,
 	setLastText,
 }: {
 	isStart: boolean;
 	setIsStart: (isStart: boolean) => void;
+	setNError: (nError: boolean) => void;
 	setLastText: (lastText: string) => void;
 }) {
 	// スタートボタンをクリックしたとき、isStartをtrueにする
 	const startClick = () => {
 		setIsStart(true);
+		setNError(false);
 		setLastText("しりとり"); // スタート時に初期単語をセット
 	};
 
@@ -48,25 +60,45 @@ function StartResetButton({
 
 // しりとり 単語の更新
 function TextUpdate({
+	isStart,
+	setIsStart,
+	nError,
+	setNError,
 	lastText,
 	setLastText,
 }: {
+	isStart: boolean;
+	setIsStart: (isStart: boolean) => void;
+	nError: boolean;
+	setNError: (nError: boolean) => void;
 	lastText: string;
 	setLastText: (lastText: string) => void;
 }) {
+	// 前回の単語の末尾と入力した単語の先頭が一致しないとき、true 一致するとき、false
 	const [siritoriError, setSiritoriError] = useState(false);
 
 	return (
 		<>
-			<div className="last-text">ひとつ前の単語は{lastText}です</div>
-			<NewText
-				lastText={lastText}
-				setLastText={setLastText}
-				setSiritoriError={setSiritoriError}
-			/>
+			{(isStart || nError) && (
+				<div className="last-text">ひとつ前の単語は{lastText}です</div>
+			)}
+			{isStart && (
+				<NewText
+					setIsStart={setIsStart}
+					lastText={lastText}
+					setLastText={setLastText}
+					setSiritoriError={setSiritoriError}
+					setNError={setNError}
+				/>
+			)}
 			{siritoriError && (
 				<div className="siritori-error">
 					ひとつ前の単語の末尾と入力した単語の先頭が一致しません
+				</div>
+			)}
+			{nError && (
+				<div className="n-error">
+					入力した単語の末尾が'ん'でした ゲームを終了します
 				</div>
 			)}
 		</>
@@ -75,14 +107,19 @@ function TextUpdate({
 
 // 単語の更新
 function NewText({
+	setIsStart,
 	lastText,
 	setLastText,
 	setSiritoriError,
+	setNError,
 }: {
+	setIsStart: (isStart: boolean) => void;
 	lastText: string;
 	setLastText: (lastText: string) => void;
 	setSiritoriError: (siritoriError: boolean) => void;
+	setNError: (nError: boolean) => void;
 }) {
+	// 入力した単語
 	const [newText, setNewText] = useState("");
 
 	// テキストボックスが変更されたら、State更新
@@ -97,6 +134,12 @@ function NewText({
 			// 前回の入力をStateにセット
 			setLastText(newText);
 			setSiritoriError(false); // 一致したらエラーを削除
+
+			// 入力した単語の末尾が'ん'のときエラーを出す
+			if (newText.slice(-1) === "ん") {
+				setIsStart(false);
+				setNError(true);
+			}
 		} else setSiritoriError(true); // 一致しなかった場合はエラーをtrueにする
 		setNewText(""); // 入力後、テキストボックスを空にする
 	};
